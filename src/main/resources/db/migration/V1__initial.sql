186c0c01fd101988211a3195fdb6566804e43eae
Garvit Joshi <garvitjoshi9@gmail.com>
2023-11-13 21:08:22 +0530

    audit table query change

--- src/main/resources/db/migration/V1__initial.sql
+++ src/main/resources/db/migration/V1__initial.sql
@@ -1,12 +1,15 @@
-CREATE TABLE audit
+CREATE SEQUENCE public.audit_id_seq START WITH 1 INCREMENT BY 1 MINVALUE 1 NO MAXVALUE CACHE 1;
+
+CREATE TABLE public.audit
 (
-    id            BIGSERIAL PRIMARY KEY,
-    uuid          VARCHAR(128) NOT NULL,
-    application   VARCHAR(32) NOT NULL,
-    caller        VARCHAR(32) NOT NULL,
-    audit_context VARCHAR(8)  NOT NULL,
-    time          TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    auditor       VARCHAR(64) NOT NULL,
+    id            BIGINT PRIMARY KEY DEFAULT nextval('public.audit_id_seq'::regclass) NOT NULL,
+    uuid          CHARACTER VARYING(128)                                              NOT NULL,
+    application   CHARACTER VARYING(32)                                               NOT NULL,
+    caller        CHARACTER VARYING(32)                                               NOT NULL,
+    audit_context CHARACTER VARYING(8)                                                NOT NULL,
+    audit_time    TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    auditor       CHARACTER VARYING(64)                                               NOT NULL,
     blob          JSONB,
     metadata      TEXT
 );
+
